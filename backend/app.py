b08ea127ab977d3a2021bc3e16f02432ef136c9d
MakaSravani <sravani.m@ealliancecorp.com>
2023-12-27 14:30:38 +0530

    commit

--- backend/app.py
+++ backend/app.py
@@ -1,77 +1,15 @@
-# import openai, os, requests
-
-# openai.api_type = "azure"
-# # Azure OpenAI on your own data is only supported by the 2023-08-01-preview API version
-# openai.api_version = "2023-08-01-preview"
-
-# # Azure OpenAI setup
-# openai.api_base = "https://cog-cdzj2obpa54em.openai.azure.com/" # Add your endpoint here
-# openai.api_key = "825a0d3880054857a94fd70649196ad3" # Add your OpenAI API key here
-# deployment_id = "chat" # Add your deployment ID here
-
-# # Azure AI Search setup
-# search_endpoint = "https://gptkb-cdzj2obpa54em.search.windows.net"; # Add your Azure AI Search endpoint here
-# search_key = "VmM0fMtVNqbwZKbomZrZgRKnadwl12Qc3KXxMsXIzIAzSeBAgDyI"; # Add your Azure AI Search admin key here
-# search_index_name = "escogidxtest"; # Add your Azure AI Search index name here
-
-# def setup_byod(deployment_id: str) -> None:
-#     """Sets up the OpenAI Python SDK to use your own data for the chat endpoint.
-
-#     :param deployment_id: The deployment ID for the model to use with your own data.
-
-#     To remove this configuration, simply set openai.requestssession to None.
-#     """
-
-#     class BringYourOwnDataAdapter(requests.adapters.HTTPAdapter):
-
-#         def send(self, request, **kwargs):
-#             request.url = f"{openai.api_base}/openai/deployments/{deployment_id}/extensions/chat/completions?api-version={openai.api_version}"
-#             return super().send(request, **kwargs)
-
-#     session = requests.Session()
-
-#     # Mount a custom adapter which will use the extensions endpoint for any call using the given `deployment_id`
-#     session.mount(
-#         prefix=f"{openai.api_base}/openai/deployments/{deployment_id}",
-#         adapter=BringYourOwnDataAdapter()
-#     )
-
-#     openai.requestssession = session
-
-# setup_byod(deployment_id)
-# def generate_message_text(user_question: str):
-#     return [{"role": "user", "content": Question}]
-
-# Question = input("Enter your Question?  ")
-# message_text = generate_message_text(Question)
-
-# completion = openai.ChatCompletion.create(
-#     messages=message_text,
-#     deployment_id=deployment_id,
-#     dataSources=[  # camelCase is intentional, as this is the format the API expects
-#         {
-#             "type": "AzureCognitiveSearch",
-#             "parameters": {
-#                 "endpoint": search_endpoint,
-#                 "key": search_key,
-#                 "indexName": search_index_name,
-#             }
-#         }
-#     ]
-# )
-# # print(completion)
-# Answer= completion['choices'][0]['message']['content']
-# print("Answer:" , Answer)
 
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,jsonify
+from flask_cors import CORS
 import openai
 import os
 import requests
 
 app = Flask(__name__)
-app.static_folder = 'static'
+CORS(app)
+# app.static_folder = 'static'
 
 openai.api_type = "azure"
 openai.api_version = "2023-08-01-preview"
@@ -99,37 +37,48 @@ def setup_byod(deployment_id: str) -> None:
     openai.requestssession = session
 
 
-def generate_message_text(user_question: str):
-    return [{"role": "user", "content": user_question}]
+def generate_message_text(conversation_history, user_question):
+    messages = [{"role": "user", "content": user_question}]
+
+    for entry in conversation_history:
+        messages.append({"role": "assistant", "content": entry["assistant_response"]})
+
+    return messages
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def index():
-    if request.method == "POST":
-        user_question = request.form["user_question"]
-        message_text = generate_message_text(user_question)
+    conversation_history = []
+    user_question = request.json.get("user_question")
 
-        completion = openai.ChatCompletion.create(
-            messages=message_text,
-            deployment_id=deployment_id,
-            dataSources=[
-                {
-                    "type": "AzureCognitiveSearch",
-                    "parameters": {
-                        "endpoint": search_endpoint,
-                        "key": search_key,
-                        "indexName": search_index_name,
-                    }
+    if conversation_history:
+        last_assistant_response = conversation_history[-1]["assistant_response"]
+        message_text = generate_message_text(conversation_history, user_question)
+        message_text.append({"role": "assistant", "content": last_assistant_response})
+    else:
+        message_text = generate_message_text(conversation_history, user_question)
+
+    completion = openai.ChatCompletion.create(
+        messages=message_text,
+        deployment_id=deployment_id,
+        dataSources=[
+            {
+                "type": "AzureCognitiveSearch",
+                "parameters": {
+                    "endpoint": search_endpoint,
+                    "key": search_key,
+                    "indexName": search_index_name,
                 }
-            ]
-        )
+            }
+        ]
+    )
 
-        assistant_response = completion['choices'][0]['message']['content']
-        return render_template("index.html", user_question=user_question, assistant_response=assistant_response)
+    assistant_response = completion['choices'][0]['message']['content']
+    conversation_history.append({"user_question": user_question, "assistant_response": assistant_response})
 
-    return render_template("index.html", user_question=None, assistant_response=None)
+    return jsonify({"user_question": user_question, "assistant_response": assistant_response})
 
 
 if __name__ == "__main__":
     setup_byod(deployment_id)
-    app.run(debug=True)
+    app.run(debug=True,port=5000)
---
+++ frontend/src/Chatapp.js
@@ -0,0 +1,59 @@
+
+
+import React, { useState } from 'react';
+import axios from 'axios';
+import './ChatApp.css';
+
+const ChatApp = () => {
+  const [userQuestion, setUserQuestion] = useState('');
+  const [conversation, setConversation] = useState([]);
+
+  const handleAskQuestion = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/', { user_question: userQuestion });
+      const { user_question, assistant_response } = response.data;
+
+      // Append the new message to the conversation
+      setConversation((prevConversation) => [
+        ...prevConversation,
+        { role: 'user', content: user_question },
+        { role: 'assistant', content: assistant_response },
+      ]);
+
+      // Clear the user input
+      setUserQuestion('');
+    } catch (error) {
+      console.error('Error asking question:', error.message);
+    }
+  };
+
+  return (
+    <div className="chat-container">
+        <div className="headings">
+        <h3>Deerfield HR Gen AI Chatbot</h3>
+        <h4>Powered by JustAskAthena.ai from eAlliance</h4>
+      </div>
+      <div className="conversation">
+      
+        <h2>Ask Anything about Deerfield HR</h2>
+        {conversation.map((message, index) => (
+          <div key={index} className={message.role === 'user' ? 'user-message' : 'assistant-message'}>
+            {message.content}
+          </div>
+        ))}
+      </div>
+      <div className="user-input">
+        <input
+          type="text"
+          id="user-question"
+          placeholder="Type your question..."
+          value={userQuestion}
+          onChange={(e) => setUserQuestion(e.target.value)}
+        />
+        <button onClick={handleAskQuestion}>Ask</button>
+      </div>
+    </div>
+  );
+};
+
+export default ChatApp;
